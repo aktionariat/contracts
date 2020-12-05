f7e8afc8465db2db0c4684e718d4afbcf7eaeb03
Luzius Meisser <luzius@meissereconomics.com>
2020-12-05 22:41:39 +0100

    custom owner when creating contracts with factory

--- src/MarketMakerFactory.sol
+++ src/MarketMakerFactory.sol
@@ -11,11 +11,11 @@ contract MarketMakerFactory {
 
   event ContractDeployed(address contractAddress);
     
-  function create(bytes32 salt, address baseCurrency, address shareToken) public returns (address) {
+  function create(bytes32 salt, address baseCurrency, address shareToken, address owner) public returns (address) {
     MarketMaker marketMaker = new MarketMaker{salt: salt}();
     marketMaker.initialize(baseCurrency, shareToken);
     emit ContractDeployed(address(marketMaker));
-    marketMaker.transferOwnership(msg.sender);
+    marketMaker.transferOwnership(owner);
     return address(marketMaker);
   }
 }
--- src/SharesFactory.sol
+++ src/SharesFactory.sol
@@ -11,10 +11,10 @@ contract SharesFactory {
 
   event ContractDeployed(address contractAddress);
 
-  function create(bytes32 salt, string memory ticker, string memory name, string memory terms) public returns (address) {
+  function create(bytes32 salt, address owner, string memory ticker, string memory name, string memory terms) public returns (address) {
     Shares shares = new Shares{salt: salt}(ticker, name, terms);
     emit ContractDeployed(address(shares));
-    shares.transferOwnership(msg.sender);
+    shares.transferOwnership(owner);
     return address(shares);
   }
 }
