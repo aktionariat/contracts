f7c3776c4d08105bf5052e4d9d258f5d54100fe0
Luzius Meisser <luzius@aktionariat.com>
2020-12-08 17:22:12 +0100

    we should set the total number of shares when creating a shares contract

--- src/SharesFactory.sol
+++ src/SharesFactory.sol
@@ -11,9 +11,10 @@ contract SharesFactory {
 
   event ContractDeployed(address contractAddress);
 
-  function create(bytes32 salt, address owner, string memory ticker, string memory name, string memory terms) public returns (address) {
+  function create(bytes32 salt, address owner, string memory ticker, string memory name, string memory terms, uint256 total) public returns (address) {
     Shares shares = new Shares{salt: salt}(ticker, name, terms);
     emit ContractDeployed(address(shares));
+    shares.setTotalShares(total);
     shares.transferOwnership(owner);
     return address(shares);
   }
