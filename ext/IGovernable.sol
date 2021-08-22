c7ce3521cac37fbeecb78a341f28282c4aa332fd
Luzius Meisser <luzius@aktionariat.com>
2021-08-22 22:07:27 +0200

    import more methods from IGovernable and IComplianceRegistry

--- ext/IGovernable.sol
+++ ext/IGovernable.sol
@@ -1,6 +1,5 @@
 /*
     Copyright (c) 2019 Mt Pelerin Group Ltd
-
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License version 3
     as published by the Free Software Foundation with the addition of the
@@ -8,7 +7,6 @@
     FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
     MT PELERIN GROUP LTD. MT PELERIN GROUP LTD DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
     OF THIRD PARTY RIGHTS
-
     This program is distributed in the hope that it will be useful, but
     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
     or FITNESS FOR A PARTICULAR PURPOSE.
@@ -18,11 +16,9 @@
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA, 02110-1301 USA, or download the license from the following URL:
     https://www.gnu.org/licenses/agpl-3.0.fr.html
-
     The interactive user interfaces in modified source and object code versions
     of this program must display Appropriate Legal Notices, as required under
     Section 5 of the GNU Affero General Public License.
-
     You can be released from the requirements of the license by purchasing
     a commercial license. Buying such a license is mandatory as soon as you
     develop commercial activities involving Mt Pelerin Group Ltd software without
@@ -30,16 +26,15 @@
     These activities include: offering paid services based/using this product to customers,
     using this product in any application, distributing this product with a closed
     source product.
-
     For more information, please contact Mt Pelerin Group Ltd at this
     address: hello@mtpelerin.com
 */
 
-pragma solidity >= 0.8;
+pragma solidity >=0.8;
 
 /**
- * @title IComplianceRegistry
- * @dev IComplianceRegistry interface
+ * @title IGovernable
+ * @dev IGovernable interface
  **/
 interface IComplianceRegistry {
 
@@ -173,4 +168,3 @@ interface IComplianceRegistry {
     uint256 amount
   );
 }
-
