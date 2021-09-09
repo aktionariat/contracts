dbe276b90775ba2d8aaa1cc87e9c35ad1cd5a189
beruf <rube@ruf2.de>
2021-09-09 11:36:24 +0200

    fix part of the tests

--- test/truffle-fixture.js
+++ test/truffle-fixture.js
@@ -7,16 +7,17 @@ const Brokerbot = artifacts.require("Brokerbot");
 const PaymentHub = artifacts.require("PaymentHub");
 
 module.exports = async(deployer) => {
-  console.log(await deployer.getNamedAccounts());
   const accounts = await deployer.getNamedAccounts();
+  const unAccounts = await deployer.getUnNamedAccounts();
+  console.log(unAccounts);
 
   const shares = await Shares.new(config.symbol, config.name, config.terms, config.totalShares, accounts.deployer);
   Shares.setAsDeployed(shares);
 
-  const draggableShares = await DraggableShares.new(config.terms, Shares.address, config.quorumBps, config.votePeriodSeconds);
+  const draggableShares = await DraggableShares.new(config.terms, shares.address, config.quorumBps, config.votePeriodSeconds);
   DraggableShares.setAsDeployed(draggableShares);
 
-  const brokerbot = await Brokerbot.new(DraggableShares.address, config.sharePrice, 0, config.baseCurrencyAddress, accounts.deployer);
+  const brokerbot = await Brokerbot.new(draggableShares.address, config.sharePrice, 0, config.baseCurrencyAddress, accounts.deployer);
   Brokerbot.setAsDeployed(brokerbot);
 
   const paymentHub = await PaymentHub.new(config.baseCurrencyAddress);
