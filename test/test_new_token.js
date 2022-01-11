320aa9ce7595fc72087d317579c06aab9e00d03a
beruf <rube@ruf2.de>
2022-01-11 11:57:08 +0100

    enhance test coverage

--- test/test_new_token.js
+++ test/test_new_token.js
@@ -1,15 +1,16 @@
-const {network, ethers, } = require("hardhat");
+const {network, ethers, getNamedAccounts} = require("hardhat");
 const { expect } = require("chai");
 const Chance = require("chance");
 
 // Shared  Config
-const config = require("../migrations/migration_config");
+const config = require("../deploy/deploy_config");
 
 describe("New Standard", () => {
   let draggable
   let shares
   let recoveryHub;
   let baseCurrency;
+  let brokerbot;
   let paymentHub;
   let forceSend;
   let offerFactory
@@ -38,16 +39,11 @@ describe("New Standard", () => {
 
   before(async () => {
     // get signers and accounts of them
-    [owner,sig1,sig2,sig3,sig4,oracle] = await ethers.getSigners();
+    [owner,sig1,sig2,sig3,sig4,] = await ethers.getSigners();
     signers = [owner,sig1,sig2,sig3,sig4];
     accounts = [owner.address,sig1.address,sig2.address,sig3.address,sig4.address];
+    oracle = owner;
     chance = new Chance();
-
-    // random test data with chance
-    name = chance.sentence({words: 3});
-    symbol = chance.word({length: chance.natural({min: 1, max: 5})}).toUpperCase();
-    terms = chance.word({length: chance.natural({min: 1, max: 10})});
-    dterms = chance.word({length: chance.natural({min: 1, max: 10})});
 
     // deploy contracts
     baseCurrency = await ethers.getContractAt("ERC20Basic",config.baseCurrencyAddress);
@@ -55,6 +51,28 @@ describe("New Standard", () => {
     forceSend = await ethers.getContractFactory("ForceSend")
       .then(factory => factory.deploy())
       .then(contract => contract.deployed());
+
+    await deployments.fixture([
+      "ReoveryHub",
+      "OfferFactory",
+      "Shares",
+      "DraggableShares", 
+      "AllowlistShares", 
+      "AllowlistDraggableShares", 
+      "PaymentHub", 
+      "Brokerbot"
+    ]);
+
+    paymentHub = await ethers.getContract("PaymentHub");
+    recoveryHub = await ethers.getContract("RecoveryHub");
+    offerFactory = await ethers.getContract("OfferFactory");
+    shares = await ethers.getContract("Shares");
+    draggable = await ethers.getContract("DraggableShares");
+    allowlistShares = await ethers.getContract("AllowlistShares");
+    allowlistDraggable = await ethers.getContract("AllowlistDraggableShares");
+    brokerbot = await ethers.getContract("Brokerbot");
+
+/*
 
     const priceFeedCHFUSD = "0x449d117117838fFA61263B61dA6301AA2a88B13A";  // ethereum mainnet
     const priceFeedETHUSD = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"; // ethereum mainnet
@@ -88,7 +106,7 @@ describe("New Standard", () => {
     allowlistDraggable = await ethers.getContractFactory("AllowlistDraggableShares")
       .then(factory => factory.deploy(dterms, allowlistShares.address, config.quorumBps, config.votePeriodSeconds, recoveryHub.address, offerFactory.address, oracle.address, owner.address))
       .then(contract => contract.deployed());
-
+*/
     
     // Mint baseCurrency Tokens (xchf) to first 5 accounts
     await network.provider.request({
@@ -126,10 +144,18 @@ describe("New Standard", () => {
       });
 
       it("Should have params specified at the constructor", async() => {
-        expect(await shares.name()).to.equal(name);
-        expect(await shares.symbol()).to.equal(symbol);
-        expect(await shares.terms()).to.equal(terms);
-      }); 
+        expect(await shares.name()).to.equal(config.name);
+        expect(await shares.symbol()).to.equal(config.symbol);
+        expect(await shares.terms()).to.equal(config.terms);
+      });
+      
+      it("Should set the right owner", async () =>{
+        expect(await shares.owner()).to.equal(owner.address);
+      });
+      
+      it("Should get right claim deleter", async () => {
+        expect(await shares.getClaimDeleter()).to.equal(owner.address);
+      });
     });
 
     describe("Draggable Shares", () => {
@@ -138,33 +164,37 @@ describe("New Standard", () => {
       });
   
       it("Should have params specified at the constructor", async() => {
-        expect(await draggable.terms()).to.equal(dterms);
+        expect(await draggable.terms()).to.equal(config.terms);
       }); 
+      
+      it("Should get right claim deleter", async () => {
+        expect(await draggable.getClaimDeleter()).to.equal(oracle.address);
+      });
     });
-
+    
     describe("AllowlistShares", () => {
       it("Should deploy allowlist shares", async () => {
         expect(allowlistShares.address).to.exist;
       });
-
+      
       it("Should have params specified at the constructor", async() => {
-        expect(await allowlistShares.name()).to.equal(name);
-        expect(await allowlistShares.symbol()).to.equal(symbol);
-        expect(await allowlistShares.terms()).to.equal(terms);
+        expect(await allowlistShares.name()).to.equal(config.allowlist_name);
+        expect(await allowlistShares.symbol()).to.equal(config.allowlist_symbol);
+        expect(await allowlistShares.terms()).to.equal(config.allowlist_terms);
       }); 
     });
-
+    
     describe("Allowlist Draggable Shares", () => {
       it("Should deploy contracts", async () => {
         expect(allowlistDraggable.address).to.exist;
       });
-  
+      
       it("Should have params specified at the constructor", async() => {
-        expect(await allowlistDraggable.terms()).to.equal(dterms);
+        expect(await allowlistDraggable.terms()).to.equal(config.allowlist_terms);
       }); 
     });
   });
-
+  
   describe("Setup", () => {
     it("should have some ETH in first 5 accounts", async () => {  
       for (let i = 0; i < 5; i++) {
@@ -172,28 +202,146 @@ describe("New Standard", () => {
         assert(!balance.isZero(), "Balance is 0");
       }
     });
-  
+    
     it("should have some BaseCurrency in first 5 accounts", async () => {
       for (let i = 0; i < 5; i++) {
         const balance = await baseCurrency.balanceOf(accounts[i]);
         assert(!balance.isZero(), "Balance is 0");
       }
     });
-
+    
     it("should have some Shares in first 5 accounts", async () => {
       for (let i = 0; i < 5; i++) {
         const balance = await shares.balanceOf(accounts[i]);
         assert(!balance.isZero(), "Balance is 0");
       }
     });
-
+    
     it("should have some DraggableShares in first 5 accounts", async () => {
       for (let i = 0; i < 5; i++) {
         const balance = await draggable.balanceOf(accounts[i]);
         assert(!balance.isZero());
       }
     });
+  });
   
+  describe("Shares", () => {
+    it("Should set custom claim collateral for shares", async () => {
+      const collateralAddress = config.baseCurrencyAddress;
+      const collateralRate = 10;
+      // test that only owenr can set
+      await expect(shares.connect(sig1).setCustomClaimCollateral(collateralAddress, collateralRate))
+      .to.be.revertedWith("not owner");
+      // test with owner
+      await shares.setCustomClaimCollateral(collateralAddress, collateralRate);
+      expect(await shares.customCollateralAddress()).to.equal(collateralAddress);
+      expect(await shares.customCollateralRate()).to.equal(collateralRate);
+    });
+    
+    it("Should change terms for shares", async () => {
+      const newTerms = "www.test.com/newterms";
+      await shares.setTerms(newTerms);
+      
+      // check if terms set correct
+      expect(await shares.terms()).to.equal(newTerms);
+    });
+
+    it("Should emit event for shares announcment", async () => {
+      const message = "Test";
+      await expect(shares.announcement(message))
+        .to.emit(shares, 'Announcement')
+        .withArgs(message);
+    });
+
+    it("Should set new name", async () => {
+      const newName = "New Shares";
+      const newSymbol = "NSHR"
+      await shares.setName(newSymbol, newName);
+      expect(await shares.name()).to.equal(newName);
+      expect(await shares.symbol()).to.equal(newSymbol);
+    })
+
+    it("Should set custom claim collateral for shares", async () => {
+      const collateralAddress = config.baseCurrencyAddress;
+      const collateralRate = 10;
+      // test that only owenr can set
+      await expect(shares.connect(sig1).setCustomClaimCollateral(collateralAddress, collateralRate))
+        .to.be.revertedWith("not owner");
+      // test with owner
+      await shares.setCustomClaimCollateral(collateralAddress, collateralRate);
+      expect(await shares.customCollateralAddress()).to.equal(collateralAddress);
+      expect(await shares.customCollateralRate()).to.equal(collateralRate);
+    });
+
+    it("Should change terms for shares", async () => {
+      const newTerms = "www.test.com/newterms";
+      await shares.setTerms(newTerms);
+
+      // check if terms set correct
+      expect(await shares.terms()).to.equal(newTerms);
+    });
+
+    it("Should set new total shares", async () => {
+      const randomChange = chance.natural({ min: 1, max: 50000 });
+      const totalSupply = await shares.totalValidSupply();
+      let newTotalShares = await totalSupply.add(randomChange);
+
+      // should revert if new total shares is < than valid supply
+      await expect(shares.setTotalShares(await totalSupply.sub(randomChange)))
+      .to.be.revertedWith("below supply");
+
+      // set correct new total and check if set correct
+      await shares.setTotalShares(totalSupply.add(randomChange));
+      const totalShares = await shares.totalShares();
+      expect(totalShares).to.equal(totalSupply.add(randomChange));
+    });
+
+    it("Should declare tokens invalid", async () => {
+      const randomdAmount = chance.natural({ min: 1, max: 50000 });
+      const invalidTokenBefore = await shares.invalidTokens();
+      const holderBalance = await shares.balanceOf(sig4.address);
+
+      // try to declare too many tokens invalid
+      await expect(shares.declareInvalid(sig4.address, holderBalance.add(1), "more than I have"))
+        .to.be.revertedWith("amount too high");
+      
+      await expect(shares.declareInvalid(sig4.address, randomdAmount, "test"))
+        .to.emit(shares, "TokensDeclaredInvalid")
+        .withArgs(sig4.address, randomdAmount, "test");
+      
+      const invalidTokenAfter = await shares.invalidTokens();
+      expect(invalidTokenBefore.add(randomdAmount)).to.equal(invalidTokenAfter);
+    });
+
+    it("Should burn shares", async () => {
+      const randomAmountToBurn = chance.natural({min:1, max: 5000});
+      const balanceBefore = await shares.balanceOf(sig3.address);
+      await shares.connect(sig3).burn(randomAmountToBurn);
+      const balanceAfter = await shares.balanceOf(sig3.address);
+      expect(balanceBefore.sub(randomAmountToBurn)).to.equal(balanceAfter);
+    });
+
+    it("Should mint and call on shares", async () => {
+      const randomAmountToMint = chance.natural({min:1, max: 5000});
+      const totalShares = await shares.totalShares();
+
+      //set new total shares as we mint more
+      await shares.setTotalShares(totalShares.add(randomAmountToMint));
+      const balanceBefore = await draggable.balanceOf(sig2.address);
+      // mint shares and wrap them in draggable
+      await shares.mintAndCall(sig2.address, draggable.address, randomAmountToMint, "0x01");
+      const balanceAfter = await draggable.balanceOf(sig2.address);
+
+      expect(balanceBefore.add(randomAmountToMint)).to.equal(balanceAfter);
+    });
+
+    it("Should wrap some more shares with transferAndCall", async () => {
+      const randomAmountToWrap = chance.natural({min: 1, max: 500});
+      const balanceBefore = await draggable.balanceOf(sig1.address);
+      await shares.connect(sig1).transferAndCall(draggable.address, randomAmountToWrap, "0x01");
+      const balanceAfter = await draggable.balanceOf(sig1.address);
+      expect(balanceBefore.add(randomAmountToWrap)).to.equal(balanceAfter);
+    })
   });
 
   describe("Recovery", () => {
@@ -285,9 +433,9 @@ describe("New Standard", () => {
       const overrides = {
         value: ethers.utils.parseEther("5.0")
       }
-      await expect(draggable.connect(sig1).makeAcquisitionOffer(ethers.utils.formatBytes32String('4'), ethers.utils.parseEther("1"), baseCurrency.address, overrides))
+      await expect(draggable.connect(sig1).makeAcquisitionOffer(ethers.utils.formatBytes32String('2'), ethers.utils.parseEther("1"), baseCurrency.address, overrides))
         .to.revertedWith("old offer better");
-      await draggable.connect(sig1).makeAcquisitionOffer(ethers.utils.formatBytes32String('4'), ethers.utils.parseEther("3"), baseCurrency.address, overrides);
+      await draggable.connect(sig1).makeAcquisitionOffer(ethers.utils.formatBytes32String('2'), ethers.utils.parseEther("2.3"), baseCurrency.address, overrides);
       
       // new offer from sig1
       const offerAfter = await draggable.offer();
@@ -311,7 +459,7 @@ describe("New Standard", () => {
       }
 
       // collect external vote
-      const externalTokens = ethers.BigNumber.from(3000000);
+      const externalTokens = ethers.BigNumber.from(100000);
       await offer.connect(oracle).reportExternalVotes(externalTokens, 0);
 
       //execute
@@ -425,4 +573,5 @@ describe("New Standard", () => {
       expect(await allowlistDraggable.canReceiveFromAnyone(defaultAddress)).to.equal(true);     
     });
   });
+
 });
