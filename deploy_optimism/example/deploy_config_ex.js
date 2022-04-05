ff7ca21389fc59ae7b47d969d603f426c64e0945
beruf <rube@ruf2.de>
2022-04-05 17:43:26 +0200

    example for new customer deployments

---
+++ deploy_optimism/example/deploy_config_ex.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Parameters used during contract development and testing
+  symbol: "EXA",
+  name: "Example ag",
+  terms: "https://example.com/investors",
+  totalShares: 11000000,
+  sharePrice: "10000000000000000",
+  increment: "0",
+  quorumBps: 7500,
+  votePeriodSeconds: 5184000,
+  baseCurrencyAddress: "0xE4F27b04cC7729901876B44f4EAA5102EC150265", // optimism xchf
+  multisigAddress: "xxx"
+};
