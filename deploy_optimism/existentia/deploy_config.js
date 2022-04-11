f3e5b3f86d50c844d4c99263ac8f2232172139dc
beruf <rube@ruf2.de>
2022-04-11 10:02:54 +0200

    deploy existentia

---
+++ deploy_optimism/existentia/deploy_config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Parameters used during contract development and testing
+  symbol: "EXEN",
+  name: "Existentia AG",
+  terms: "https://www.existentia.earth/investors",
+  totalShares: 10000000,
+  sharePrice: "10000000000000000",
+  increment: "0",
+  quorumBps: 7500,
+  votePeriodSeconds: 5184000,
+  baseCurrencyAddress: "0xE4F27b04cC7729901876B44f4EAA5102EC150265", // optimism xchf
+  multisigAddress: "0x670027f45A294941475D72bB86412a3373BB23e3"
+};
