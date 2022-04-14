77365a2d49ec18d4e0c8263e43d63cdbe1695236
beruf <rube@ruf2.de>
2022-04-14 10:56:32 +0200

    deploy allowlist example

--- deploy_optimism/example-allowlist/deploy_allowlist_shares_ex.js
+++ deploy_optimism/example-allowlist/deploy_allowlist_shares_ex.js
@@ -7,45 +7,45 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
   const { deployer } = await getNamedAccounts();
 
   const owner = config.multisigAddress;
-  const recoveryHub = await deployments.get("RecoveryHub");
 
+  const recoveryHub = await deployments.get("RecoveryHub");
+  
   const symbol = config.symbol;
   const name = config.name;
   const terms = config.terms;
   const totalShares = config.totalShares;
-  
+
   if (network.name != "hardhat") {
     console.log("-----------------------")
-    console.log("Deploy Shares "+ config.symbol)
+    console.log("Deploy Allowlist Shares " + config.symbol)
     console.log("-----------------------")
     console.log("deployer: %s", deployer);
-    console.log("recoveryHub: %s", recoveryHub.address);
-    console.log("owner: %s", owner); // don't forget to set it in deploy_config.js as the multsigadr
-
+    console.log("owner: %s", owner)  // don't forget to set it in deploy_config.js as the multsigadr
+    
     const prompt = await new Confirm("Addresses correct?").run();
     if(!prompt) {
       console.log("exiting");
       process.exit();
     }
   }
-
+  
   const feeData = await ethers.provider.getFeeData();
 
-  const { address } = await deploy("Shares"+config.symbol, {
-    contract: "Shares",
+  const { address } = await deploy("AllowlistShares"+config.symbol, {
+    contract: "AllowlistShares",
     from: deployer,
     args: [
       symbol,
       name,
       terms,
       totalShares,
-      owner,
-      recoveryHub.address],
+      recoveryHub.address,
+      owner],
     log: true,
     maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
     maxFeePerGas: feeData.maxFeePerGas
   });
 };
 
-module.exports.tags = ["Shares"+config.symbol];
+module.exports.tags = ["AllowlistShares"+config.symbol];
 module.exports.dependencies = ["RecoveryHub"];
