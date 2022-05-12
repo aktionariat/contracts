143cdc8469cbe664d9b9b41e5ef06382cfd5342f
beruf <rube@ruf2.de>
2022-05-12 18:29:43 +0200

    add brokerbot registry contract and test

---
+++ deploy/deploy_brokerbot_registry.js
@@ -0,0 +1,35 @@
+const Confirm = require('prompt-confirm');
+
+module.exports = async function ({ ethers, deployments, getNamedAccounts, network }) {
+  const { deploy } = deployments;
+
+  const { deployer, owner } = await getNamedAccounts();
+    
+  if (network.name != "hardhat") {
+    console.log("-----------------------")
+    console.log("Deploy Brokerbot DAI")
+    console.log("-----------------------")
+    console.log("deployer: %s", deployer);
+    console.log("owner: %s", owner);  // don't forget to set it in hardhat.config.js as the multsig account
+
+    const prompt = await new Confirm("Addresses correct?").run();
+    if(!prompt) {
+      console.log("exiting");
+      process.exit();
+    }
+  }
+
+  const feeData = await ethers.provider.getFeeData();
+
+  const { address } = await deploy("BrokerbotRegistry", {
+    contract: "BrokerbotRegistry",
+    from: deployer,
+    args: [
+      owner],
+    log: true,
+    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+    maxFeePerGas: feeData.maxFeePerGas
+  });
+};
+
+module.exports.tags = ["BrokerbotRegistry"];
