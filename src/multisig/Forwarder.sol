b6da9409eecfda67225331396d98d0d3847b767f
beruf <rube@ruf2.de>
2022-12-07 16:06:17 +0100

    fix compile error

--- src/multisig/Forwarder.sol
+++ src/multisig/Forwarder.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.8.0;
 
 contract Forwarder {
 
-    function(address payable target) public payable {
-        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+    function fallback(address payable target) public payable {
+        (bool sent, bytes memory data) = target.call{value: msg.value}("");
         require(sent, "Failed to send Ether");
     }
 
