fb792091b3942d0872b77d667280d9d7ab1a0847
beruf <rube@ruf2.de>
2022-12-20 14:20:26 +0100

    ignore forwarder

--- .solcover.js
+++ .solcover.js
@@ -1,9 +1,11 @@
 module.exports = {
   skipFiles: [
     'multisig/MultiSigWallet.sol', //old version
-    'multisig/MultiSigWalletV2.sol',//tested in backend
+    'multisig/MultiSigWalletV2.sol',//old version
     'multisig/Nonce.sol', //part of multisig that is tested in backend
     'multisig/RLPEncode.sol', //part of multisig that is tested in backend
-    'utils/Address.sol'
+    'multisig/Forwarder.sol', // coverage has problem with fallback
+    'utils/Address.sol',
+    'employee/EmployeeShares.sol' // not in use 
    ]
 };
