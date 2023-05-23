770c0d90b6966ed8d40efdb094de016c7ecc3d26
beruf <rube@ruf2.de>
2023-05-23 10:36:02 +0200

    remove console import

--- src/ERC20/ERC20Flaggable.sol
+++ src/ERC20/ERC20Flaggable.sol
@@ -15,7 +15,6 @@ pragma solidity ^0.8.0;
 import "./IERC20.sol";
 import "./ERC20Errors.sol";
 import "./IERC677Receiver.sol";
-import "hardhat/console.sol";
 /**
  * @dev Implementation of the `IERC20` interface.
  *
