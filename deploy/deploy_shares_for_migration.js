6c29cb11555b093b4d8e344a3e3a142adf974563
beruf <rube@ruf2.de>
2023-07-03 17:05:36 +0200

    changes for rename of old AF token

--- deploy/deploy_shares_for_migration.js
+++ deploy/deploy_shares_for_migration.js
@@ -5,20 +5,22 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
 
   const { deployer, owner } = await getNamedAccounts();
 
-  const recoveryHub = await deployments.get("RecoveryHub");
+  //const recoveryHub = await deployments.get("RecoveryHub");
+  const recoveryHub = "0xc6AfD3b605957b9BA94143F59d451c657F507516";
 
-  const symbol = "MS";
-  const name = "Migration Shares";
-  const terms = "migration.ch/terms";
-  const totalShares = 10000000;
+
+  const symbol = "OLD";
+  const name = "Invalid Alan Frei";
+  const terms = "Invalid terms";
+  const totalShares = 0;
   
-  if (network.name != "hardhat") {
+  if (network.name != "heardhat") {
     console.log("-----------------------")
     console.log("Deploy SharesMigration")
     console.log("-----------------------")
     console.log("deployer: %s", deployer);
-    console.log("recoveryHub: %s", recoveryHub.address);
-    console.log("owner: %s", owner); // don't forget to set it in the hardhat config
+    console.log("recoveryHub: %s", recoveryHub);
+    console.log("owner: %s", deployer); // don't forget to set it in the hardhat config
 
     const prompt = await new Confirm("Addresses correct?").run();
     if(!prompt) {
@@ -37,8 +39,8 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
       name,
       terms,
       totalShares,
-      owner,
-      recoveryHub.address],
+      deployer,
+      recoveryHub],
     log: true,
     maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
     maxFeePerGas: feeData.maxFeePerGas
@@ -46,4 +48,3 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
 };
 
 module.exports.tags = ["SharesMigration"];
-module.exports.dependencies = ["RecoveryHub"];
