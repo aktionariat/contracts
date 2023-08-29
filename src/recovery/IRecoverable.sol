f4701c5f6d439c31065e89bb428ee08e489941f5
beruf <bernhard@aktionariat.com>
2023-08-29 14:29:04 +0200

    Update typo
    Co-authored-by: Mathias Scherer <scherer.mat@gmail.com>

--- src/recovery/IRecoverable.sol
+++ src/recovery/IRecoverable.sol
@@ -9,7 +9,7 @@ interface IRecoverable is IERC20{
 	/*//////////////////////////////////////////////////////////////
                             Custom errors
     //////////////////////////////////////////////////////////////*/
-    /// The new customm claim collateral rate has to be always > 0. 
+    /// The new custom claim collateral rate has to be always > 0. 
     error Recoverable_RateZero();
     /// Failguard when an erc20 transfer returns false. 
     error Recoverable_TransferFailed();
