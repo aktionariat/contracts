b5aed61c14d90ed8b04f6039de38184c0a020a44
beruf <rube@ruf2.de>
2024-03-06 15:44:46 +0100

    deploy: exa zchf brokerbot

--- deploy/redeploy-bb/deploy_brokerbot_ex.js
+++ deploy/redeploy-bb/deploy_brokerbot_ex.js
@@ -8,8 +8,8 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts, networ
 
   const owner = config.multisigAddress;
   //const shares = await deployments.get('Shares');
-  const shares = "0x6f38e0f1a73c96cB3f42598613EA3474F09cB200"
-  const paymentHub = "0xbB55b164b641cD0eC89E901884d79f920f310605";
+  const shares = config.sharesAddress;
+  const paymentHub = config.paymentHubAddress;
   
   const price = config.sharePrice;
   const increment = config.increment;
--- deploy/redeploy-bb/deploy_config.js
+++ deploy/redeploy-bb/deploy_config.js
@@ -1,8 +1,10 @@
 module.exports = {
   // Parameters used during contract development and testing
-  symbol: "DAKS",
-  sharePrice: "14000000000000000000",
+  symbol: "EXA",
+  sharePrice: "10000000000000000",
   increment: "10000000000000",
-  baseCurrencyAddress: "0xB4272071eCAdd69d933AdcD19cA99fe80664fc08", // mainent xchf
-  multisigAddress: "0x4Fd9DbA1d53B7E6cC933a2Fdd12B1c012a0654F6"
+  baseCurrencyAddress: "0xB58E61C3098d85632Df34EecfB899A1Ed80921cB", // mainent zchf
+  multisigAddress: "0xB8020D1656caDab89F9d3ad638c4015B6B705143",
+  paymentHubAddress: "0xaf1A5a633A31f8659F06e32da7b41E207AdAd43C",
+  sharesAddress: "0xbf0e13ab37573ABB68ff9C33Ee9FF8148b8a61E2"
 };
