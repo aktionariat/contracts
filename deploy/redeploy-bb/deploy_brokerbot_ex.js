9d889e90e65ce9a6fa9f4909aa121b1e39667316
beruf <rube@ruf2.de>
2024-04-26 16:03:51 +0200

    refactor: all addresses to config

--- deploy/redeploy-bb/deploy_brokerbot_ex.js
+++ deploy/redeploy-bb/deploy_brokerbot_ex.js
@@ -8,8 +8,8 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts, networ
 
   const owner = config.multisigAddress;
   //const shares = await deployments.get('Shares');
-  const shares = "0x2e880962a9609aa3eab4def919fe9e917e99073b"
-  const paymentHub = "0xaf1A5a633A31f8659F06e32da7b41E207AdAd43C";
+  const shares = config.shareAddress;
+  const paymentHub = config.paymentHubAddress;
   
   const price = config.sharePrice;
   const increment = config.increment;
--- deploy/redeploy-bb/deploy_config.js
+++ deploy/redeploy-bb/deploy_config.js
@@ -1,6 +1,10 @@
+const { paymentHubAddress } = require("../../scripts/deploy_config_mainnet");
+
 module.exports = {
   // Parameters used during contract development and testing
   symbol: "BOSS",
+  shareAddress: "0x2e880962a9609aa3eab4def919fe9e917e99073b",
+  paymentHubAddress: "0xaf1A5a633A31f8659F06e32da7b41E207AdAd43C", // one WITHOUT the new path for payFromEther
   sharePrice: "10202370000000000000",
   increment: "5000000000000",
   // baseCurrencyAddress: "0xB4272071eCAdd69d933AdcD19cA99fe80664fc08", // mainent xchf
