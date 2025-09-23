c4d3b82004dd65760348cd3b702373b4d6741f5b
Murat Ögat <murat@aktionariat.com>
2025-09-24 00:32:06 +0200

    migrating 2 more tests

--- test/MultichainSharesMaster.ts
+++ test/MultichainSharesMaster.ts
@@ -1,27 +1,14 @@
-import hre, { ethers } from "hardhat";
 import { expect } from "chai";
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import TestModule from "../ignition/modules/TestModule";
-import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 import { Contract } from "ethers";
-
-async function deployTestModuleFixture() {
-  return hre.ignition.deploy(TestModule);
-}
+import { connection } from "./TestBase.ts";
+import TestModule from "../ignition/modules/TestModule.ts";
 
 describe("MultichainSharesMaster", function () {
-  let deployer: HardhatEthersSigner;
-  let owner: HardhatEthersSigner;
-  let signer1: HardhatEthersSigner, signer2: HardhatEthersSigner, signer3: HardhatEthersSigner, signer4: HardhatEthersSigner, signer5: HardhatEthersSigner;
   let allowlistShares: Contract;
   let multichainSharesMaster: Contract;
   
   before(async function() {
-    [deployer, owner, signer1, signer2, signer3, signer4, signer5] = await ethers.getSigners();
-  });
-
-  beforeEach(async function() {
-    ({ allowlistShares, multichainSharesMaster } = await loadFixture(deployTestModuleFixture));
+    ({ allowlistShares, multichainSharesMaster } = await connection.ignition.deploy(TestModule));
   });
 
   it("Should deploy", async function () {
@@ -29,7 +16,6 @@ describe("MultichainSharesMaster", function () {
   });
 
   it("Set wrapped token correctly", async function () {
-    ({ allowlistShares, multichainSharesMaster } = await loadFixture(deployTestModuleFixture));
     expect(await multichainSharesMaster.wrapped()).to.be.equal(allowlistShares);
   });
 });
