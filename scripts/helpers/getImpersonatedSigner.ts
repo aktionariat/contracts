2aa786633f0957e6482cd7735917e3fe9bf1ec40
Murat Ögat <murat@aktionariat.com>
2025-09-23 08:24:20 +0200

    added ignition to gitignore

--- scripts/helpers/getImpersonatedSigner.ts
+++ scripts/helpers/getImpersonatedSigner.ts
@@ -1,12 +1,11 @@
-import hre from "hardhat";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/types";
+import { NetworkConnection } from "hardhat/types/network";
 
-export async function getImpersonatedSigner(impersonatedAddress: string): Promise<HardhatEthersSigner> {
-    const { provider, ethers } = await hre.network.connect();
-    await provider.request({
+export async function getImpersonatedSigner(impersonatedAddress: string, connection: NetworkConnection): Promise<HardhatEthersSigner> {
+    await connection.provider.request({
         method: "hardhat_impersonateAccount",
         params: [impersonatedAddress],
     });
-    const signer = ethers.getSigner(impersonatedAddress);
+    const signer = connection.ethers.getSigner(impersonatedAddress);
     return signer;
 }
