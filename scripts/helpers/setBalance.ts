23911d296defe4bd260f2103ddcf31331bd0acea
Murat Ögat <murat@aktionariat.com>
2025-11-26 15:51:31 +0100

    Update setBalance.ts

--- scripts/helpers/setBalance.ts
+++ scripts/helpers/setBalance.ts
@@ -24,6 +24,10 @@ async function setERC20Balance(contractAddress: string, address: string, slot: n
     setStorageAt(contractAddress, index, amountBytesStr);
 }
 
+export async function getStorageAt(address: string, position: bigint) {
+    return await provider.request({method: "eth_getStorageAt", params: [address, "0x" + position.toString(16), "latest"]});
+};
+
 const setStorageAt = async (address: string, index: string, value: string) => {
     await provider.request({method: "hardhat_setStorageAt", params: [address, index, value]});
     await provider.request({method: "hardhat_mine"}); // Just mines to the next block
