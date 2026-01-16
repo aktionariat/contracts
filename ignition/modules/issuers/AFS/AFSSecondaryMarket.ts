b587d0367445cb4509773a3de54f3bcffbb66cd6
Murat Ögat <murat@aktionariat.com>
2026-01-16 09:47:03 +0100

    Deployed AFS SecondaryMarket

---
+++ ignition/modules/issuers/AFS/AFSSecondaryMarket.ts
@@ -0,0 +1,21 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const config = {
+  tradeReactorAddress: "0x4a67f44F48a887910525Dc8004ce804b99BC27cA",
+  secondaryMarketFactoryAddress: "0x08C2cb730dC666Dd6487667B1D66BA9eF5Fe71D4",
+  zchfAddress: "0xB58E61C3098d85632Df34EecfB899A1Ed80921cB",
+  backendRouterAddress: "0x59f0941e75f2F77cA4577E48c3c5333a3F8D277b",
+  afsAddress: "0x56528C1dF17FD5451451eB6EFDE297758bc8f9a1"
+}
+
+const AFSSecondaryMarket260116Module = buildModule("AFSSecondaryMarket260116Module", (m) => {
+  const backendOwner = m.getAccount(0);
+
+  const secondaryMarketFactory = m.contractAt("SecondaryMarketFactory", config.secondaryMarketFactoryAddress);
+  m.call(secondaryMarketFactory, "deploy", [backendOwner, config.zchfAddress, config.afsAddress, config.tradeReactorAddress, config.backendRouterAddress], { from: backendOwner });
+  return { 
+    secondaryMarketFactory
+   };
+});
+
+export default AFSSecondaryMarket260116Module;
